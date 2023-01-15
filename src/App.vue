79cc962228dff70ac2c037b0d96906d498a8acb1
jasiel <jasiel_serra@hotmail.com>
2023-01-15 15:38:51 -0300

    Getter for API.

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,42 @@
 <template>
-
-<!-- Content -->
     <div class="px-3 py-10 md:px-10">
         <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
-            <TodoSpinner/>
-            <TodoFormAdd/>
-            <TodoItems/>
-            <!-- Todo empty -->
-            <div class="text-center text-lg text-gray-500">
-                Você ainda não tem nenhuma tarefa.
-            </div>
-            <!--/ Todo empty -->
+          <pre>
+             {{ todos }}
+          </pre>
+            <TodoSpinner v-if="loading" />
+
+            <template v-else>
+           <!--     <TodoFormAdd />
+
+                <TodoItems />
+
+                <TodoEmpty /> -->
+            </template>
         </div>
     </div>
-    <!--/ Content -->
-
-
 </template>
 
 <script>
+import TodoSpinner from '@/components/TodoSpinner';
+import TodoFormAdd from '@/components/TodoFormAdd';
+import TodoItems from '@/components/TodoItems';
+import TodoEmpty from '@/components/TodoEmpty';
+import axios from 'axios';
 
-import TodoSpinner from "@/components/TodoSpinner";
-import TodoFormAdd from "@/components/TodoFormAdd";
 export default {
-  name: 'App',
-  components: {
-    TodoFormAdd,
-    TodoSpinner
+    name: 'App',
+    components: { TodoEmpty, TodoItems, TodoFormAdd, TodoSpinner },
 
-  }
+    data() {
+        return {
+            todos: []
+        }
+    },
+
+    created() {
+        axios.get('http://localhost:3000/todos')
+            .then((response) => { this.todos = response.data })
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/TodoItems.vue
+++ src/components/TodoItems.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="space-y-2">
-        <TodoItem
+     <!--   <TodoItem
             v-for="todo in $store.state.todos"
             :key="todo.id"
             :todo="todo"
-        />
+        /> -->
     </div>
 </template>
 
 <script>
 import TodoItem from '@/components/TodoItem';
 export default {
-    components: { TodoItem }
+//    components: { TodoItem }
 }
 </script>

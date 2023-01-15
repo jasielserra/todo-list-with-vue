313a6592e9ab652627c4f35cba497e54de6ef7bb
jasiel <jasiel_serra@hotmail.com>
2023-01-15 17:54:00 -0300

    Simulate promise

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { createStore } from 'vuex'
+import axios from "axios";
+
+export default createStore({
+  state: {
+    todos: []
+  },
+
+  mutations: {
+    storeTodos(state, payload) {
+      state.todos = payload
+    },
+
+  },
+  actions: {
+    getTodos({ commit }) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+              return axios.get('http://localhost:3000/todos')
+            .then((response) => {
+                commit('storeTodos', response.data)
+                resolve()
+            })
+        },3000)
+        })
+    },
+   },
+  getters: {
+
+  },
+  modules: {
+  }
+})

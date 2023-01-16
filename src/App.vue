bdbc5c20ba36ca2bad0be334845890fdd6744e10
jasiel <jasiel_serra@hotmail.com>
2023-01-16 06:11:30 -0300

    Update title and completed todo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="px-3 py-10 md:px-10">
         <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
-          <pre>
-             {{ $store.state.todos }}
-          </pre>
+
             <TodoSpinner v-if="loading" />
 
             <template v-else>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -4,7 +4,7 @@
 border-gray-400 last:border-b-0">
             <div class="flex items-center justify-center
 mr-2">
-                <button class="text-gray-400">
+                <button :class="{ 'text-gray-400': isCompleted, 'text-gray-400': !isCompleted }" @click="onCheckClick">
                     <svg class="w-5 h-5" fill="none"
                          stroke="currentColor" viewBox="0 0 24 24"
                          xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round"
@@ -16,10 +16,11 @@ mr-2">
                 <input
                     type="text"
                     placeholder="Digite a sua tarefa"
-                    :value="todo.title"
+                    :value="title"
                     class="bg-gray-300 placeholder-gray-500
 text-gray-700 font-light focus:outline-none block w-full appearance-none
 leading-normal mr-3"
+                @keyup.enter="onTitleChange"
                 >
             </div>
 
@@ -57,5 +58,48 @@ export default {
             default: () => ({}),
         },
     },
+
+  data(){
+     return {
+       title: this.todo.title,
+       isCompleted: this.todo.completed,
+     }
+  },
+
+  methods: {
+    onTitleChange() {
+
+      if (!this.title) {
+        return;
+      }
+
+      this.updateTodo()
+
+    },
+
+
+    updateTodo(){
+
+        const payload = {
+        id: this.todo.id,
+        data: {
+          title: this.title,
+          completed: this.isCompleted
+        }
+
+      }
+
+      this.$store.dispatch('updateTodo', payload)
+  //    console.log($evt.target.value);
+    },
+
+
+
+    onCheckClick(){
+      this.isCompleted = !this.isCompleted
+      this.updateTodo()
+    }
+  },
+
 }
 </script>
